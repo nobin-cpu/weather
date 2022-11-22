b361e2eb35bf26785e230970de95ea50c0095d35
nobin-cpu <nnobin611@gmail.com>
2022-11-22 22:08:02 +0600

    my changes

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -89,168 +89,170 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 0, 29, 44),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(),
-        child: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Column(
-              children: [
-                Container(
-                  height: MediaQuery.of(context).size.height * .60,
-                  width: MediaQuery.of(context).size.width,
-                  margin: EdgeInsets.only(left: 10, right: 10),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(22),
-                          bottomRight: Radius.circular(22)),
-                      gradient: LinearGradient(
-                          colors: [
-                            Color.fromARGB(255, 78, 146, 255),
-                            Color.fromARGB(255, 238, 2, 255)
-                          ],
-                          stops: [
-                            0.3,
-                            0.86
-                          ],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter)),
-                  child: Column(children: [
-                    SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Image.network(
-                              height: MediaQuery.of(context).size.height * .1,
-                              weatherMap!["weather"][0]["main"] == "Clear"
-                                  ? "images/sun2.png"
-                                  : weatherMap!["weather"][0]["main"] == "rainy"
-                                      ? "images/rainy.png"
-                                      : "images/cloudy.png"),
-                          Container(
-                            margin:
-                                const EdgeInsets.only(left: 20.0, right: 10.0),
-                            height: 40,
-                            width: 2,
-                            color: Colors.black,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              children: [
-                                Text(
-                                  "${Jiffy(DateTime.now()).format("MMM do yyyy,   h:mm: a")} ",
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                                Text(
-                                  "${weatherMap!["name"]}",
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Align(
-                      heightFactor: MediaQuery.of(context).size.height * .003,
-                      alignment: Alignment.center,
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "Temp:${weatherMap!["main"]["temp"]}.",
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            SizedBox(
-                              height: 5,
-                            ),
-                            Text(
-                              "Feels like:${weatherMap!["main"]["feels_like"]}.",
-                              style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              "Sky:${weatherMap!["weather"][0]["main"]}",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            SizedBox(
-                              height: 5,
-                            ),
-                            Text(
-                                "Humidity:${forecastMap!["list"][0]["main"]["humidity"]} ,pressure:${forecastMap!["list"][0]["main"]["pressure"]}",
-                                style: TextStyle(color: Colors.white)),
-                            SizedBox(
-                              height: 5,
-                            ),
-                            Text(
-                                "Sunrise ${Jiffy(DateTime.fromMillisecondsSinceEpoch(weatherMap!["sys"]["sunrise"] * 1000)).format("h:mm:a")} ,Sunset:${Jiffy(DateTime.fromMillisecondsSinceEpoch(weatherMap!["sys"]["sunset"] * 1000)).format("h:mm:a")} ",
-                                style: TextStyle(color: Colors.white)),
-                          ]),
-                    ),
-                  ]),
-                ),
-                Align(
-                  heightFactor: MediaQuery.of(context).size.height * .002,
-                  alignment: Alignment.bottomCenter,
-                  child: SizedBox(
-                    height: 150,
-                    child: ListView.builder(
+      body: SafeArea(top: true,
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(),
+          child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height * .60,
+                    width: MediaQuery.of(context).size.width,
+                    margin: EdgeInsets.only(left: 10, right: 10),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(22),
+                            bottomRight: Radius.circular(22)),
+                        gradient: LinearGradient(
+                            colors: [
+                              Color.fromARGB(255, 78, 146, 255),
+                              Color.fromARGB(255, 238, 2, 255)
+                            ],
+                            stops: [
+                              0.3,
+                              0.86
+                            ],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter)),
+                    child: Column(children: [
+                      SingleChildScrollView(
                         scrollDirection: Axis.horizontal,
-                        shrinkWrap: true,
-                        itemCount: forecastMap!.length,
-                        itemBuilder: (context, index) {
-                          return Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                              margin: EdgeInsets.only(right: 2),
-                              color: Color.fromARGB(255, 46, 0, 17),
-                              width: 80,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Image.asset(
+                                height: MediaQuery.of(context).size.height * .1,
+                                weatherMap!["weather"][0]["main"] == "Clear"
+                                    ? "images/sun2.png"
+                                    : weatherMap!["weather"][0]["main"] == "rainy"
+                                        ? "images/rainy.png"
+                                        : "images/cloudy.png"),
+                            Container(
+                              margin:
+                                  const EdgeInsets.only(left: 20.0, right: 10.0),
+                              height: 40,
+                              width: 2,
+                              color: Colors.black,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
                               child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
                                 children: [
                                   Text(
-                                    "${Jiffy("${forecastMap!["list"][index]["dt_txt"]}").format("EEE h:mm")}",
+                                    "${Jiffy(DateTime.now()).format("MMM do yyyy,   h:mm: a")} ",
                                     style: TextStyle(color: Colors.white),
                                   ),
                                   Text(
-                                      "${forecastMap!["list"][index]["main"]["temp_min"]}",
-                                      style: TextStyle(color: Colors.white)),
-                                  Text(
-                                      "${forecastMap!["list"][index]["main"]["temp_max"]}",
-                                      style: TextStyle(color: Colors.white)),
-                                  Align(
-                                    alignment: Alignment.bottomCenter,
-                                    child: Padding(
-                                      padding: EdgeInsets.all(8.0),
-                                      child: Text(
-                                          "${forecastMap!["list"][index]["weather"][0]["description"]}",
-                                          style:
-                                              TextStyle(color: Colors.white)),
-                                    ),
-                                  )
+                                    "${weatherMap!["name"]}",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
                                 ],
                               ),
                             ),
-                          );
-                        }),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Align(
+                        heightFactor: MediaQuery.of(context).size.height * .003,
+                        alignment: Alignment.center,
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Temp:${weatherMap!["main"]["temp"]}.",
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                "Feels like:${weatherMap!["main"]["feels_like"]}.",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                "Sky:${weatherMap!["weather"][0]["main"]}",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                  "Humidity:${forecastMap!["list"][0]["main"]["humidity"]} ,pressure:${forecastMap!["list"][0]["main"]["pressure"]}",
+                                  style: TextStyle(color: Colors.white)),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                  "Sunrise ${Jiffy(DateTime.fromMillisecondsSinceEpoch(weatherMap!["sys"]["sunrise"] * 1000)).format("h:mm:a")} ,Sunset:${Jiffy(DateTime.fromMillisecondsSinceEpoch(weatherMap!["sys"]["sunset"] * 1000)).format("h:mm:a")} ",
+                                  style: TextStyle(color: Colors.white)),
+                            ]),
+                      ),
+                    ]),
                   ),
-                )
-              ],
-            )),
+                  Align(
+                    heightFactor: MediaQuery.of(context).size.height * .002,
+                    alignment: Alignment.bottomCenter,
+                    child: SizedBox(
+                      height: 150,
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: true,
+                          itemCount: forecastMap!.length,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                margin: EdgeInsets.only(right: 2),
+                                color: Color.fromARGB(255, 46, 0, 17),
+                                width: 80,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      "${Jiffy("${forecastMap!["list"][index]["dt_txt"]}").format("EEE h:mm")}",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                    Text(
+                                        "${forecastMap!["list"][index]["main"]["temp_min"]}",
+                                        style: TextStyle(color: Colors.white)),
+                                    Text(
+                                        "${forecastMap!["list"][index]["main"]["temp_max"]}",
+                                        style: TextStyle(color: Colors.white)),
+                                    Align(
+                                      alignment: Alignment.bottomCenter,
+                                      child: Padding(
+                                        padding: EdgeInsets.all(8.0),
+                                        child: Text(
+                                            "${forecastMap!["list"][index]["weather"][0]["description"]}",
+                                            style:
+                                                TextStyle(color: Colors.white)),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            );
+                          }),
+                    ),
+                  )
+                ],
+              )),
+        ),
       ),
     );
   }
